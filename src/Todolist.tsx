213623e061414d9cebe2c07bbbcf30454df28f05
Anna Krasilnikova <akr6363@mail.ru>
2023-04-07 17:57:05 +0300

    ts dop 4 start

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -8,14 +8,14 @@ export type TasksType = {
 }
 
 type PropsType = {
-    id: string
+    id: number
     title: string
     tasks: Array<TasksType>
-    removeTask: (taskId: string, todolistId: string) => void
-    changeFilter: (value: FilterValuesType, todolistId: string) => void
-    addTask: (title: string, todolistId: string) => void
-    changeTaskStatus: (id: string, isDone: boolean, todolistId: string) => void
-    removeTodolist: (id: string) => void
+    removeTask: (taskId: string, todolistId: number) => void
+    changeFilter: (value: FilterValuesType, todolistId: number) => void
+    addTask: (title: string, todolistId: number) => void
+    changeTaskStatus: (id: string, isDone: boolean, todolistId: number) => void
+    removeTodolist: (id: number) => void
     filter: FilterValuesType
 }
 
